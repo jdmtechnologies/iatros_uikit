12cd24ece731b2292fd1b6175a9eebdd417e7a7d
jeison vargas <jeisonvargas@flutterlabapps.com>
2026-02-01 21:01:36 -0500

    init

--- lib/widgets/index_widget.dart
+++ lib/widgets/index_widget.dart
@@ -1,3 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:iatros_uikit/models/blood_type.dart';
+import 'package:iatros_uikit/models/gender_type.dart';
+import 'package:iatros_uikit/models/input_type.dart';
 import 'package:iatros_uikit/widgets/primary_button_widget.dart';
 import 'package:iatros_uikit/widgets/secondary_button_widget.dart';
 import 'package:iatros_uikit/widgets/icon_button_widget.dart';
@@ -20,43 +25,426 @@ import 'package:iatros_uikit/widgets/background_example_widget.dart';
 import 'package:iatros_uikit/utils/loading_overlay.dart';
 
 class UiBackgrounds {
-  Type get medicalBackground => UiMedicalBackground;
-  Type get simpleMedicalBackground => UiSimpleMedicalBackground;
-  Type get backgroundExample => UiBackgroundExample;
+  Widget medicalBackground({
+    Key? key,
+    required Widget child,
+    bool showBotanicalElements = true,
+  }) =>
+      UiMedicalBackground(
+        key: key,
+        showBotanicalElements: showBotanicalElements,
+        child: child,
+      );
+
+  Widget simpleMedicalBackground({
+    Key? key,
+    required Widget child,
+    bool showElements = true,
+  }) =>
+      UiSimpleMedicalBackground(
+        key: key,
+        showElements: showElements,
+        child: child,
+      );
+
+  Widget backgroundExample({Key? key}) => UiBackgroundExample(key: key);
 }
 
 class UiButtons {
-  Type get primaryButton => UiPrimaryButton;
-  Type get secondaryButton => UiSecondaryButton;
-  Type get iconButton => UiIconButton;
-  Type get imageButton => UiImageButton;
+  Widget primaryButton({
+    Key? key,
+    required String label,
+    required VoidCallback onPressed,
+    double? width,
+    double? height,
+    bool isLoading = false,
+    bool isDisabled = false,
+  }) =>
+      UiPrimaryButton(
+        key: key,
+        label: label,
+        onPressed: onPressed,
+        width: width,
+        height: height,
+        isLoading: isLoading,
+        isDisabled: isDisabled,
+      );
+
+  Widget secondaryButton({
+    Key? key,
+    required String label,
+    required VoidCallback onPressed,
+    double? width,
+    double? height,
+    bool isLoading = false,
+    bool isDisabled = false,
+  }) =>
+      UiSecondaryButton(
+        key: key,
+        label: label,
+        onPressed: onPressed,
+        width: width,
+        height: height,
+        isLoading: isLoading,
+        isDisabled: isDisabled,
+      );
+
+  Widget iconButton({
+    Key? key,
+    required IconData icon,
+    required VoidCallback onPressed,
+    bool isLoading = false,
+    bool isDisabled = false,
+    Color? backgroundColor,
+    Color? iconColor,
+    double size = 24,
+    String? tooltip,
+  }) =>
+      UiIconButton(
+        key: key,
+        icon: icon,
+        onPressed: onPressed,
+        isLoading: isLoading,
+        isDisabled: isDisabled,
+        backgroundColor: backgroundColor,
+        iconColor: iconColor,
+        size: size,
+        tooltip: tooltip,
+      );
+
+  Widget imageButton({
+    Key? key,
+    required VoidCallback onTap,
+    double size = 100,
+    String? image,
+    IconData? icon,
+  }) =>
+      UiImageButton(
+        key: key,
+        onTap: onTap,
+        size: size,
+        image: image ?? '',
+        icon: icon,
+      );
 }
 
 class UiCards {
-  Type get baseCard => UiBaseCard;
-  Type get glassmorphismCard => UiGlassmorphismCard;
+  Widget baseCard({
+    Key? key,
+    required Widget child,
+    EdgeInsetsGeometry? padding,
+    EdgeInsetsGeometry? margin,
+    Color? backgroundColor,
+    double? elevation,
+    BorderRadius? borderRadius,
+    VoidCallback? onTap,
+  }) =>
+      UiBaseCard(
+        key: key,
+        padding: padding,
+        margin: margin,
+        backgroundColor: backgroundColor,
+        elevation: elevation,
+        borderRadius: borderRadius,
+        onTap: onTap,
+        child: child,
+      );
+
+  Widget glassmorphismCard({
+    Key? key,
+    required Widget child,
+    double blur = 10.0,
+    double opacity = 0.1,
+    EdgeInsetsGeometry? padding,
+    BorderRadius? borderRadius,
+    Color? backgroundColor,
+    BoxBorder? border,
+  }) =>
+      UiGlassmorphismCard(
+        key: key,
+        blur: blur,
+        opacity: opacity,
+        padding: padding,
+        borderRadius: borderRadius,
+        backgroundColor: backgroundColor,
+        border: border,
+        child: child,
+      );
 }
 
 class UiInputs {
-  Type get textInput => UiTextInput;
-  Type get passwordInput => UiPasswordInput;
-  Type get datePickerInput => UiDatePickerInput;
-  Type get identificationSelector => UiIdentificationSelector;
-  Type get genderSelector => UiGenderSelector;
-  Type get bloodTypeSelector => UiBloodTypeSelector;
-  Type get imagePickerInput => UiImagePickerInput;
-  Type get filePickerInput => UiFilePickerInput;
-  Type get multiSelectDropdown => UiMultiSelectDropdown;
+  Widget textInput({
+    Key? key,
+    String? hint,
+    String? label,
+    int? maxLines,
+    InputType type = InputType.dark,
+    int? maxLength,
+    bool isRequired = false,
+    bool isReadOnly = false,
+    bool obscureText = false,
+    String? errorText,
+    Widget? prefixIcon,
+    Widget? suffixIcon,
+    VoidCallback? onTap,
+    TextInputType? keyboardType,
+    ValueChanged<String>? onChanged,
+    TextEditingController? controller,
+    String? Function(String?)? validator,
+    List<TextInputFormatter>? inputFormatters,
+  }) =>
+      UiTextInput(
+        key: key,
+        hint: hint,
+        label: label,
+        maxLines: maxLines,
+        type: type,
+        maxLength: maxLength,
+        isRequired: isRequired,
+        isReadOnly: isReadOnly,
+        obscureText: obscureText,
+        errorText: errorText,
+        prefixIcon: prefixIcon,
+        suffixIcon: suffixIcon,
+        onTap: onTap,
+        keyboardType: keyboardType,
+        onChanged: onChanged,
+        controller: controller,
+        validator: validator,
+        inputFormatters: inputFormatters,
+      );
+
+  Widget passwordInput({
+    Key? key,
+    String? hint,
+    String? label,
+    InputType type = InputType.light,
+    bool isRequired = false,
+    String? errorText,
+    TextEditingController? controller,
+    ValueChanged<String>? onChanged,
+    String? Function(String?)? validator,
+  }) =>
+      UiPasswordInput(
+        key: key,
+        hint: hint,
+        label: label,
+        type: type,
+        isRequired: isRequired,
+        errorText: errorText,
+        controller: controller,
+        onChanged: onChanged,
+        validator: validator,
+      );
+
+  Widget datePickerInput({
+    Key? key,
+    String? label,
+    DateTime? firstDate,
+    DateTime? lastDate,
+    DateTime? initialDate,
+    ValueNotifier<DateTime?>? selectedDateNotifier,
+    ValueChanged<DateTime>? onDateSelected,
+    bool isRequired = false,
+    String? errorText,
+    InputType type = InputType.dark,
+  }) =>
+      UiDatePickerInput(
+        key: key,
+        label: label,
+        firstDate: firstDate,
+        lastDate: lastDate,
+        initialDate: initialDate,
+        selectedDateNotifier: selectedDateNotifier,
+        onDateSelected: onDateSelected,
+        isRequired: isRequired,
+        errorText: errorText,
+        type: type,
+      );
+
+  Widget identificationSelector({
+    Key? key,
+    ValueNotifier<String?>? selectedTypeNotifier,
+    TextEditingController? numberController,
+    ValueChanged<String?>? onTypeChanged,
+    ValueChanged<String>? onNumberChanged,
+    String? errorText,
+    bool isRequired = false,
+    InputType type = InputType.dark,
+  }) =>
+      UiIdentificationSelector(
+        key: key,
+        selectedTypeNotifier: selectedTypeNotifier,
+        numberController: numberController,
+        onTypeChanged: onTypeChanged,
+        onNumberChanged: onNumberChanged,
+        errorText: errorText,
+        isRequired: isRequired,
+        type: type,
+      );
+
+  Widget genderSelector({
+    Key? key,
+    ValueNotifier<Gender?>? selectedGenderNotifier,
+    ValueChanged<Gender?>? onChanged,
+    String? errorText,
+    bool isRequired = false,
+    InputType type = InputType.dark,
+  }) =>
+      UiGenderSelector(
+        key: key,
+        selectedGenderNotifier: selectedGenderNotifier,
+        onChanged: onChanged,
+        errorText: errorText,
+        isRequired: isRequired,
+        type: type,
+      );
+
+  Widget bloodTypeSelector({
+    Key? key,
+    ValueNotifier<BloodType?>? selectedBloodTypeNotifier,
+    ValueChanged<BloodType?>? onChanged,
+    String? errorText,
+    bool isRequired = false,
+    InputType type = InputType.dark,
+  }) =>
+      UiBloodTypeSelector(
+        key: key,
+        selectedBloodTypeNotifier: selectedBloodTypeNotifier,
+        onChanged: onChanged,
+        errorText: errorText,
+        isRequired: isRequired,
+        type: type,
+      );
+
+  Widget imagePickerInput({
+    Key? key,
+    String? label,
+    String? hint,
+    String? errorText,
+    dynamic selectedImage,
+    required ValueChanged<dynamic> onImageSelected,
+    bool isRequired = false,
+    double? maxWidth,
+    double? maxHeight,
+    InputType type = InputType.dark,
+  }) =>
+      UiImagePickerInput(
+        key: key,
+        label: label,
+        hint: hint,
+        errorText: errorText,
+        selectedImage: selectedImage,
+        onImageSelected: onImageSelected,
+        isRequired: isRequired,
+        maxWidth: maxWidth,
+        maxHeight: maxHeight,
+        type: type,
+      );
+
+  Widget filePickerInput({
+    Key? key,
+    String? label,
+    String? hint,
+    String? errorText,
+    dynamic selectedFile,
+    required ValueChanged<dynamic> onFileSelected,
+    bool isRequired = false,
+    List<String>? allowedExtensions,
+    String? fileName,
+  }) =>
+      UiFilePickerInput(
+        key: key,
+        label: label,
+        hint: hint,
+        errorText: errorText,
+        selectedFile: selectedFile,
+        onFileSelected: onFileSelected,
+        isRequired: isRequired,
+        allowedExtensions: allowedExtensions,
+        fileName: fileName,
+      );
+
+  Widget multiSelectDropdown<T>({
+    Key? key,
+    String? label,
+    String? hint,
+    required List<T> options,
+    required List<T> selectedItems,
+    required String Function(T) displayText,
+    required ValueChanged<List<T>> onChanged,
+    bool isRequired = false,
+    bool isMultiselect = true,
+    String? errorText,
+    Future<void> Function(String)? onSearch,
+  }) =>
+      UiMultiSelectDropdown<T>(
+        key: key,
+        label: label,
+        hint: hint,
+        options: options,
+        selectedItems: selectedItems,
+        displayText: displayText,
+        onChanged: onChanged,
+        isRequired: isRequired,
+        isMultiselect: isMultiselect,
+        errorText: errorText,
+        onSearch: onSearch,
+      );
 }
 
 class UiLogo {
-  Type get iatrosLogo => UiIatrosLogo;
-  Type get iatrosLogoVertical => UiIatrosLogoVertical;
-  Type get iatrosLogoIcon => UiIatrosLogoIcon;
+  Widget iatrosLogo({
+    Key? key,
+    double? width,
+    double? height,
+    bool showText = true,
+    Color? textColor,
+    double? textSize,
+  }) =>
+      UiIatrosLogo(
+        key: key,
+        width: width,
+        height: height,
+        showText: showText,
+        textColor: textColor,
+        textSize: textSize,
+      );
+
+  Widget iatrosLogoVertical({
+    Key? key,
+    double? width,
+    double? height,
+  }) =>
+      UiIatrosLogoVertical(
+        key: key,
+        width: width,
+        height: height,
+      );
+
+  Widget iatrosLogoIcon({
+    Key? key,
+    double? size,
+    Color? color,
+  }) =>
+      UiIatrosLogoIcon(
+        key: key,
+        size: size,
+        color: color,
+      );
 }
 
 class UiPopUp {
-  Type get loadingOverlay => LoadingOverlay;
+  Widget loadingOverlay({
+    Key? key,
+    required bool isLoading,
+    required Widget child,
+  }) =>
+      LoadingOverlay(
+        key: key,
+        isLoading: isLoading,
+        child: child,
+      );
 }
 
 /// Agrupa todos los widgets del paquete.
