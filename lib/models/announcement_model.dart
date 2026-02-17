fc4db93b8c11fde94f39907c4b9b2134102003fe
jeison vargas <jeisonvargas@flutterlabapps.com>
2026-02-17 09:41:48 -0500

    implemented useresExcluded

--- lib/models/announcement_model.dart
+++ lib/models/announcement_model.dart
@@ -5,12 +5,14 @@ class AnnouncementModel {
     final String title;
     final DateTime dateEnd;
     final String description;
+    final List<String> useresExcluded;
 
     AnnouncementModel({
         required this.id,
         required this.title,
         required this.dateEnd,
         required this.description,
+        required this.useresExcluded,
     });
 
     AnnouncementModel copyWith({
@@ -18,12 +20,14 @@ class AnnouncementModel {
         String? title,
         DateTime? dateEnd,
         String? description,
+        List<String>? useresExcluded,
     }) => 
         AnnouncementModel(
             id: id ?? this.id,
             title: title ?? this.title,
             dateEnd: dateEnd ?? this.dateEnd,
             description: description ?? this.description,
+            useresExcluded: useresExcluded ?? this.useresExcluded,
         );
 
     factory AnnouncementModel.fromRawJson(String str) => AnnouncementModel.fromJson(json.decode(str));
@@ -35,6 +39,7 @@ class AnnouncementModel {
         title: json["title"],
         description: json["description"],
         dateEnd: DateTime.parse(json["date_end"]),
+        useresExcluded: List<String>.from(json["useres_excluded"].map((x) => x)),
     );
 
     factory AnnouncementModel.init() => AnnouncementModel(
@@ -42,6 +47,7 @@ class AnnouncementModel {
         title: "",
         description: "",
         dateEnd: DateTime.now(),
+        useresExcluded: [],
     );
 
     Map<String, dynamic> toJson() => {
@@ -49,5 +55,6 @@ class AnnouncementModel {
         "title": title,
         "description": description,
         "date_end": dateEnd.toIso8601String(),
+        "useres_excluded": useresExcluded.map((e) => e).toList(),
     };
 }
