98f9f9a2224e1f4968c505db551104f7cdc5f16b
jeison vargas <jeisonvargas@flutterlabapps.com>
2026-02-20 17:15:04 -0500

    implemented time input

---
+++ lib/models/health_administrators_model.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+class HealthAdministratorsModel {
+    final String codigo;
+    final String nombre;
+
+    HealthAdministratorsModel({
+        required this.codigo,
+        required this.nombre,
+    });
+
+    HealthAdministratorsModel copyWith({
+        String? codigo,
+        String? nombre,
+    }) => 
+        HealthAdministratorsModel(
+            codigo: codigo ?? this.codigo,
+            nombre: nombre ?? this.nombre,
+        );
+
+    factory HealthAdministratorsModel.fromRawJson(String str) => HealthAdministratorsModel.fromJson(json.decode(str));
+
+    String toRawJson() => json.encode(toJson());
+
+    factory HealthAdministratorsModel.fromJson(Map<String, dynamic> json) => HealthAdministratorsModel(
+        codigo: json["Codigo"],
+        nombre: json["Nombre"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "Codigo": codigo,
+        "Nombre": nombre,
+    };
+}
